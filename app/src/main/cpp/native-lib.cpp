00a89061193cd3f754b14560711ef014ea82dd48
Hector Ricardo Mendez Sordia <hectorricardomendez@gmail.com>
2020-10-10 21:36:16 -0500

    Integrating FluidSynth to app

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,32 @@
 #include <jni.h>
 #include <string>
+#include <fluidsynth.h>
+#include <unistd.h>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_fluidsynthandroidhelloworld_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+extern "C" JNIEXPORT void JNICALL Java_com_example_fluidsynthandroidhelloworld_MainActivity_fluidsynthHelloWorld(JNIEnv* env, jobject, jstring jSoundfontPath) {
+    // Setup synthesizer
+    fluid_settings_t *settings = new_fluid_settings();
+    fluid_synth_t *synth = new_fluid_synth(settings);
+    fluid_audio_driver_t *adriver = new_fluid_audio_driver(settings, synth);
+
+    // Load sample soundfont
+    const char* soundfontPath = env->GetStringUTFChars(jSoundfontPath, nullptr);
+    fluid_synth_sfload(synth, soundfontPath, 1);
+
+    fluid_synth_noteon(synth, 0, 60, 127); // play middle C
+    sleep(1); // sleep for 1 second
+    fluid_synth_noteoff(synth, 0, 60); // stop playing middle C
+
+    fluid_synth_noteon(synth, 0, 62, 127);
+    sleep(1);
+    fluid_synth_noteoff(synth, 0, 62);
+
+    fluid_synth_noteon(synth, 0, 64, 127);
+    sleep(1);
+    fluid_synth_noteoff(synth, 0, 64);
+
+    // Clean up
+    delete_fluid_audio_driver(adriver);
+    delete_fluid_synth(synth);
+    delete_fluid_settings(settings);
 }
--- app/src/main/java/com/example/fluidsynthandroidhelloworld/MainActivity.java
+++ app/src/main/java/com/example/fluidsynthandroidhelloworld/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.fluidsynthandroidhelloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.widget.TextView;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,14 +21,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        try {
+            String tempSoundfontPath = copyAssetToTmpFile("sndfnt.sf2");
+            fluidsynthHelloWorld(tempSoundfontPath);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String copyAssetToTmpFile(String fileName) throws IOException {
+        try (InputStream is = getAssets().open(fileName)) {
+            String tempFileName = "tmp_" + fileName;
+            try (FileOutputStream fos = openFileOutput(tempFileName, Context.MODE_PRIVATE)) {
+                int bytes_read;
+                byte[] buffer = new byte[4096];
+                while ((bytes_read = is.read(buffer)) != -1) {
+                    fos.write(buffer, 0, bytes_read);
+                }
+            }
+            return getFilesDir() + "/" + tempFileName;
+        }
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    public native void fluidsynthHelloWorld(String soundfontPath);
 }
